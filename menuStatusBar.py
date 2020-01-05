1c54ae0b4ad15753ead5ddc4d4c58090363c7c82
krystian <krystian.zasada@op.pl>
2020-01-05 22:44:46 +0100

    Errors when closing docks

--- menuStatusBar.py
+++ menuStatusBar.py
@@ -28,17 +28,17 @@ class MenuBar:
         self.list_charts = docks_visiable.addAction("List of charts")
         self.list_charts.setCheckable(True)
         self.list_charts.setChecked(True)
-        self.list_charts.toggled.connect(lambda: self.show_dock(0))
+        self.list_charts.triggered.connect(lambda: self.show_dock(0))
 
         self.Data = docks_visiable.addAction("Data")
         self.Data.setCheckable(True)
         self.Data.setChecked(True)
-        self.Data.toggled.connect(lambda: self.show_dock(1))
+        self.Data.triggered.connect(lambda: self.show_dock(1))
 
         self.Settings = docks_visiable.addAction("Settings")
         self.Settings.setCheckable(True)
         self.Settings.setChecked(True)
-        self.Settings.toggled.connect(lambda: self.show_dock(2))
+        self.Settings.triggered.connect(lambda: self.show_dock(2))
 
         self.showDock.dock_widget_chart.visibilityChanged.connect(lambda: self.change_checked_docks(0))
         self.showDock.dockWidget_data.visibilityChanged.connect(lambda: self.change_checked_docks(1))
@@ -50,13 +50,13 @@ class MenuBar:
         message.setText(" If you found bugs, inform me:                  ")
         message.setIcon(QMessageBox.Information)
         message.setStandardButtons(QMessageBox.Ignore)
-        message.addButton(QPushButton('Gtihub'), QMessageBox.YesRole)
-        message.setInformativeText("\n -Email: chart.bugreport@gmail.com \n \n -Gtihub: (click the button)")
+        message.addButton(QPushButton('GtiHub'), QMessageBox.YesRole)
+        message.setInformativeText("\n -Email: chart.bugreport@gmail.com \n \n -GtiHub: (click the button)")
         message.buttonClicked.connect(self.github_link)
         message.exec_()
 
     def github_link(self, i):
-        if i.text() == 'Gtihub':
+        if i.text() == 'GtiHub':
             webbrowser.open('https://github.com/kryzasada/Chart')
 
     def show_dock(self, number):
@@ -78,12 +78,18 @@ class MenuBar:
     def change_checked_docks(self, number):
         if self.showDock.dock_widget_chart.isVisible() is False and number == 0:
             self.list_charts.setChecked(False)
+        elif self.showDock.dock_widget_chart.isVisible() is True and number == 0:
+            self.list_charts.setChecked(True)
 
         elif self.showDock.dockWidget_data.isVisible() is False and number == 1:
             self.Data.setChecked(False)
+        elif self.showDock.dock_widget_chart.isVisible() is True and number == 0:
+            self.list_charts.setChecked(True)
 
         elif self.showDock.dockWidget_settings.isVisible() is False and number == 2:
             self.Settings.setChecked(False)
+        elif self.showDock.dock_widget_chart.isVisible() is True and number == 0:
+            self.list_charts.setChecked(True)
 
 
 class StatusBar:
