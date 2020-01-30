1fb88145765fc36d742f6e8378b178d1ce86f5a8
krystian <krystian.zasada@op.pl>
2020-01-30 18:04:26 +0100

    Create first time configuration window

--- main.py
+++ main.py
@@ -3,62 +3,44 @@
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+import linecache
 
-from PyQt5 import QtCore, QtGui, QtWidgets, QtQuickWidgets
-import sys, linecache
-import dock, menuStatusBar, chartList
+import window
 
 
+def window2_create():
+    MainWindow2 = QMainWindow()
+    window2 = window.main()
+    window2.contain(MainWindow2)
+    MainWindow2.show()
 
 
-class UiMainWindow(object):
-    def setupUi(self, main_window):
+try:
+    file_line = linecache.getline("settings.txt", 3)
+    if file_line == "First start = 1\n":
+        old_file = open("settings.txt").read()
+        old_file = old_file.replace('First start = 1', 'First start = 0')
+        new_file = open("settings.txt", 'w')
+        new_file.write(old_file)
+        new_file.close()
 
-        if 0:
-            style_css = "Theme/darkorange.css"
-            style = open(style_css, "r")
-            app.setStyleSheet(style.read())
+        app1 = QApplication(sys.argv)
+        MainWindow1 = QMainWindow()
+        window1 = window.firstConfiguration()
+        window1.contain(MainWindow1)
+        window1.button_start.clicked.connect(lambda: MainWindow1.close())
+        window1.button_start.clicked.connect(lambda: window2_create())
+        MainWindow1.show()
+        sys.exit(app1.exec_())
 
-        try:
-            file_line = linecache.getline("settings.txt", 3)
-            if file_line == "First start = 1\n":
-                print("Open first time configuration window")
+    else:
+        app2 = QApplication(sys.argv)
+        window2_create()
+        sys.exit(app2.exec_())
 
-                old_file = open("settings.txt").read()
-                old_file = old_file.replace('First start = 1', 'First start = 0')
-                new_file = open("settings.txt", 'w')
-                new_file.write(old_file)
-                new_file.close()
 
-        finally:
-            linecache.clearcache()
+finally:
+    linecache.clearcache()
 
-        main_window.resize(800, 600)
-        main_window.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
-        main_window.setTabShape(QtWidgets.QTabWidget.Triangular)
-        main_window.setWindowIcon(QtGui.QIcon("Image/Icons/blank-logo.ico"))
-        main_window.setWindowTitle("GenChart")
-
-        self.centralwidget = QtWidgets.QWidget(main_window)
-        self.centralwidget.setObjectName("centralwidget")
-        main_window.setCentralWidget(self.centralwidget)
-
-        self.central_layout = QtWidgets.QVBoxLayout(self.centralwidget)
-
-        showDock = dock.Dock(main_window, self.central_layout)
-        showDock.right_up()
-        showDock.right_down()
-        showDock.left_chart()
-
-        menu_bar = menuStatusBar.MenuBar(main_window, showDock)
-        status_bar = menuStatusBar.StatusBar(main_window)
-
-        QtCore.QMetaObject.connectSlotsByName(main_window)
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = QtWidgets.QMainWindow()
-    ui = UiMainWindow()
-    ui.setupUi(main_window)
-    main_window.show()
-    sys.exit(app.exec_())
