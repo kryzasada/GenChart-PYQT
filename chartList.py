5d5e1d10cd9dec003fe2f3d06de8d568fdae6120
krystian <krystian.zasada@op.pl>
2020-02-15 19:07:42 +0100

    Simplifies checking of selected chart

--- chartList.py
+++ chartList.py
@@ -29,7 +29,7 @@ class Chart:
         self.default_pie_button.setText(linecache.getline("Language/Language.txt", 203)[:-1])
         self.default_pie_button.setMaximumSize(QtCore.QSize(110, 23))
         self.page_1_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.default_pie_button)
-        self.default_pie_button.clicked.connect(lambda: self.basic_pie_click())
+        self.default_pie_button.clicked.connect(lambda: self.chart_click("basic_pie"))
 
         self.default_pie_image = QtWidgets.QLabel(self.page_1)
         self.default_pie_image.setPixmap(QtGui.QPixmap('Image/Basic_pie_chart.png'))
@@ -41,7 +41,7 @@ class Chart:
         self.donut_pie_button.setText(linecache.getline("Language/Language.txt", 204)[:-1])
         self.donut_pie_button.setMaximumSize(QtCore.QSize(110, 23))
         self.page_1_layout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.donut_pie_button)
-        self.donut_pie_button.clicked.connect(lambda: self.donut_pie_click())
+        self.donut_pie_button.clicked.connect(lambda: self.chart_click("donut_pie"))
 
         self.donut_pie_image = QtWidgets.QLabel(self.page_1)
         self.donut_pie_image.setPixmap(QtGui.QPixmap('Image/Donut_pie_chart.png'))
@@ -53,7 +53,7 @@ class Chart:
         self.percent_pie_button.setText("Percentage\ndonut")
         self.percent_pie_button.setMaximumSize(QtCore.QSize(110, 46))
         self.page_1_layout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.percent_pie_button)
-        #self.percent_pie_button.clicked.connect(lambda: self.donut_pie_click())
+        self.percent_pie_button.clicked.connect(lambda: self.chart_click("percent_pie"))
 
         self.percent_pie_image = QtWidgets.QLabel(self.page_1)
         self.percent_pie_image.setPixmap(QtGui.QPixmap('Image/Donut_pie_chart.png'))
@@ -71,7 +71,7 @@ class Chart:
         self.default_bar_button.setText(linecache.getline("Language/Language.txt", 211)[:-1])
         self.default_bar_button.setMaximumSize(QtCore.QSize(110, 23))
         self.page_2_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.default_bar_button)
-        self.default_bar_button.clicked.connect(lambda: self.basic_bar_click())
+        self.default_bar_button.clicked.connect(lambda: self.chart_click("basic_bar"))
 
         self.default_bar_image = QtWidgets.QLabel(self.page_2)
         self.default_bar_image.setPixmap(QtGui.QPixmap('Image/Basic_bar_chart.png'))
@@ -89,7 +89,7 @@ class Chart:
         self.default_line_button.setText(linecache.getline("Language/Language.txt", 218)[:-1])
         self.default_line_button.setMaximumSize(QtCore.QSize(110, 23))
         self.page_3_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.default_line_button)
-        self.default_line_button.clicked.connect(lambda: self.basic_line_click())
+        self.default_line_button.clicked.connect(lambda: self.chart_click("basic_line"))
 
         self.default_line_image = QtWidgets.QLabel(self.page_3)
         self.default_line_image.setPixmap(QtGui.QPixmap('Image/Basic_line_chart.png'))
@@ -102,109 +102,72 @@ class Chart:
         #self.page_3.setHidden(True)
         #self.tab_widget.currentChanged.connect(lambda: self.page_3.setHidden(False))
 
-    def basic_pie_click(self):
-        self.stacked_Widget_data.setCurrentIndex(1)
-        self.stacked_widget_settings.setCurrentIndex(1)
-        dock.dock_data[1].chart_type = 'Basic_pie'
+    def chart_click(self, type):
+        for object in (self.default_pie_button,
+                       self.donut_pie_button,
+                       self.percent_pie_button,
+                       self.default_bar_button,
+                       self.default_line_button):
 
-        dock.dock_data[1].upper_TextValue_check.setEnabled(False)
-        dock.dock_data[1].upper_first_value_check.setEnabled(False)
+            if object.isEnabled() is False:
+                object.setEnabled(True)
+
+        if type == "basic_pie":
+            self.stacked_Widget_data.setCurrentIndex(1)
+            self.stacked_widget_settings.setCurrentIndex(1)
+            dock.dock_data[1].chart_type = 'Basic_pie'
+            self.default_pie_button.setEnabled(False)
+
+            name_display = True
+            value_display = False
+
+        elif type == "donut_pie":
+            self.stacked_Widget_data.setCurrentIndex(1)
+            self.stacked_widget_settings.setCurrentIndex(1)
+            dock.dock_data[1].chart_type = 'Donut_pie'
+            self.donut_pie_button.setEnabled(False)
+
+            name_display = True
+            value_display = False
+
+        elif type == "percent_pie":
+            self.stacked_Widget_data.setCurrentIndex(1)
+            self.stacked_widget_settings.setCurrentIndex(1)
+            dock.dock_data[1].chart_type = 'Percent_pie'
+            self.percent_pie_button.setEnabled(False)
+
+            name_display = True
+            value_display = False
+
+        elif type == "basic_bar":
+            self.stacked_Widget_data.setCurrentIndex(1)
+            self.stacked_widget_settings.setCurrentIndex(2)
+            dock.dock_data[1].chart_type = 'Basic_bar'
+            self.default_bar_button.setEnabled(False)
+
+            name_display = False
+            value_display = True
+
+        elif type == "basic_line":
+            self.stacked_Widget_data.setCurrentIndex(1)
+            self.stacked_widget_settings.setCurrentIndex(3)
+            dock.dock_data[1].chart_type = 'Basic_line'
+            self.default_line_button.setEnabled(False)
+
+            name_display = False
+            value_display = False
+
+        else:
+            name_display = False
+            value_display = False
+
+        dock.dock_data[1].upper__TextName_check.setEnabled(name_display)
+        dock.dock_data[1].upper_first_Name_check.setEnabled(name_display)
         for x in dock.dock_data[1].add_data:
-            x.second_block_value_check.setEnabled(False)
+            x.second_block_Name_check.setEnabled(name_display)
 
-        dock.dock_data[1].upper__TextName_check.setEnabled(True)
-        dock.dock_data[1].upper_first_Name_check.setEnabled(True)
+        dock.dock_data[1].upper_TextValue_check.setEnabled(value_display)
+        dock.dock_data[1].upper_first_value_check.setEnabled(value_display)
         for x in dock.dock_data[1].add_data:
-            x.second_block_Name_check.setEnabled(True)
-
-        self.default_pie_button.setEnabled(False)
-
-        if self.donut_pie_button.isEnabled() is False:
-            self.donut_pie_button.setEnabled(True)
-
-        elif self.default_bar_button.isEnabled() is False:
-            self.default_bar_button.setEnabled(True)
-
-        elif self.default_line_button.isEnabled() is False:
-            self.default_line_button.setEnabled(True)
-
-    def donut_pie_click(self):
-        self.stacked_Widget_data.setCurrentIndex(1)
-        self.stacked_widget_settings.setCurrentIndex(1)
-        dock.dock_data[1].chart_type = 'Donut_pie'
-
-        dock.dock_data[1].upper_TextValue_check.setEnabled(False)
-        dock.dock_data[1].upper_first_value_check.setEnabled(False)
-        for x in dock.dock_data[1].add_data:
-            x.second_block_value_check.setEnabled(False)
-
-        dock.dock_data[1].upper__TextName_check.setEnabled(True)
-        dock.dock_data[1].upper_first_Name_check.setEnabled(True)
-        for x in dock.dock_data[1].add_data:
-            x.second_block_Name_check.setEnabled(True)
-
-        self.donut_pie_button.setEnabled(False)
-
-        if self.default_pie_button.isEnabled() is False:
-            self.default_pie_button.setEnabled(True)
-
-        elif self.default_bar_button.isEnabled() is False:
-            self.default_bar_button.setEnabled(True)
-
-        elif self.default_line_button.isEnabled() is False:
-            self.default_line_button.setEnabled(True)
-
-    def basic_bar_click(self):
-        self.stacked_Widget_data.setCurrentIndex(1)
-        self.stacked_widget_settings.setCurrentIndex(2)
-        dock.dock_data[1].chart_type = 'Basic_bar'
-
-        dock.dock_data[1].upper_TextValue_check.setEnabled(True)
-        dock.dock_data[1].upper_first_value_check.setEnabled(True)
-        for x in dock.dock_data[1].add_data:
-            x.second_block_value_check.setEnabled(True)
-
-        dock.dock_data[1].upper__TextName_check.setEnabled(False)
-        dock.dock_data[1].upper_first_Name_check.setEnabled(False)
-        for x in dock.dock_data[1].add_data:
-            x.second_block_Name_check.setEnabled(False)
-
-        self.default_bar_button.setEnabled(False)
-
-        if self.default_pie_button.isEnabled() is False:
-            self.default_pie_button.setEnabled(True)
-
-        elif self.donut_pie_button.isEnabled() is False:
-            self.donut_pie_button.setEnabled(True)
-
-        elif self.default_line_button.isEnabled() is False:
-            self.default_line_button.setEnabled(True)
-
-    def basic_line_click(self):
-        self.stacked_Widget_data.setCurrentIndex(1)
-        self.stacked_widget_settings.setCurrentIndex(3)
-        dock.dock_data[1].chart_type = 'Basic_line'
-
-        dock.dock_data[1].upper_TextValue_check.setEnabled(False)
-        dock.dock_data[1].upper_first_value_check.setEnabled(False)
-        for x in dock.dock_data[1].add_data:
-            x.second_block_value_check.setEnabled(False)
-
-        dock.dock_data[1].upper__TextName_check.setEnabled(False)
-        dock.dock_data[1].upper_first_Name_check.setEnabled(False)
-        for x in dock.dock_data[1].add_data:
-            x.second_block_Name_check.setEnabled(False)
-
-        self.default_line_button.setEnabled(False)
-
-        if self.default_pie_button.isEnabled() is False:
-            self.default_pie_button.setEnabled(True)
-
-        elif self.donut_pie_button.isEnabled() is False:
-            self.donut_pie_button.setEnabled(True)
-
-        elif self.default_bar_button.isEnabled() is False:
-            self.default_bar_button.setEnabled(True)
-
-
+            x.second_block_value_check.setEnabled(value_display)
 
